72913757139e3038c8c87263f96f9e98f647e834
SHIRA BEN ZAKEN <67286268+ShiraBenZaken@users.noreply.github.com>
2020-10-14 16:43:27 +0300

    Create server.py
    using socket

---
+++ server/server.py
@@ -0,0 +1,14 @@
+
+from socket import AF_INET, socket, SOCK_STREAM
+from threading import Thread
+
+HOST = 'localhost'
+PORT = 55000
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+SERVER = socket(AF_INET, SOCK_STREAM)
+SERVER.bind(ADDR)
+
+
+
