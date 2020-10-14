edd69c93f3183b9ee20b2c84be159bd895612fcc
SHIRA BEN ZAKEN <67286268+ShiraBenZaken@users.noreply.github.com>
2020-10-14 20:35:56 +0300

    add person.py and more updates

--- server/server.py
+++ server/server.py
@@ -1,16 +1,24 @@
-
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
+import time
 
+#Global variable
 HOST = 'localhost'
 PORT =  55000
 BUFSIZ = 512
 ADDR = (HOST, PORT)
-
+MAX_CONECTION = 5
 SERVER = socket(AF_INET, SOCK_STREAM)
 SERVER.bind(ADDR)
 
+def broudcast()
+
 def client_communicate(client):
+    """
+    Tread to handle all masages from client
+    :param client: socket
+    :return: None
+    """
     run = True
     while run:
         msg = client.recv(BUFSIZ)
@@ -21,7 +29,7 @@ def client_communicate(client):
 
 
 
-def wait_for_connect(SERVER):
+def wait_for_connect():
     """
     wait for connect from new client and start new thread
     :param SERVER: socket
@@ -31,16 +39,17 @@ def wait_for_connect(SERVER):
     while run:
         try:
             client, addr =SERVER.accept()
+            print(f"[CONNECTION] {addr} connected to the server at {time.time()}")
             Thread(target=client_communicate, args=(client,)).start()
         except Exception as e:
             print("[FAILED]",e)
             run = False
-
+    print("SERVER CRASHED")
 
 if __name__ == '__main__':
-    SERVER.listen(5)
-    print("Waiting for conection...")
-    ACCEPT_THEARD =Thread(target=wait_for_connect(SERVER))
+    SERVER.listen(MAX_CONECTION) #listen for max conections
+    print("[STARTED] Waiting for conection...")
+    ACCEPT_THEARD =Thread(target=wait_for_connect)
     ACCEPT_THEARD.start()
     ACCEPT_THEARD.join()
     SERVER.close()
